0c818462bbf52d9be872abde8acf19c7da458033
WinterCricket <hodhodhod2003@gmail.com>
2017-06-07 23:05:21 -0500

    add three methods of bind, execute, and resultset

--- Database.php
+++ Database.php
@@ -26,6 +26,40 @@ class Database{
 			$this->error = getMessage();
 		}
 	}
+
+	public function query($query){
+		$this->stmt = $this->dbh->prepare($query);
+
+	}
+
+	public function bind($param, $value, $type = null){
+		if(is_null($type)){
+			switch(true){
+				case is_int($value):
+					$type = PDO::PARAM_INT;
+					break;
+				case is_bool($value):	
+					$type = PDO::PARAM_BOOL;
+					break;
+				case is_null($value):	
+					$type = PDO::PARAM_NULL;
+					break;
+					default:
+					$type = PDO::PARAM_STR;
+				
+
+			}
+
+		}
+		$this->stmt->bindValue($param, $value, $type);
+	}
+	public function execute(){
+		return $this->stmt->execute();
+	}
+	public function resultset(){
+		$this->execute();
+		return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
 }
 
 ?>
