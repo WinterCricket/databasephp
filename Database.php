e5997765410c5666b2824d5798f1e10f43b36bb7
WinterCricket <hodhodhod2003@gmail.com>
2017-06-18 22:42:29 -0500

    Add revisable data capacity

--- Database.php
+++ Database.php
@@ -1,35 +1,32 @@
-<?php 
-
+<?php
 class Database{
-	private $host = "localhost";
-	private $use = "root";
-	private $pass = "root";
-	private $dbname = "phpwebsite";
+	private $host	= 'localhost';
+	private $user	= 'root';
+	private $pass	= 'root';
+	private $dbname	= 'phpwebsite';
 
 	private $dbh;
 	private $error;
 	private $stmt;
 
-
 	public function __construct(){
-		//set DSN
-		$dsn = "mysql:host=".$this->host."dbname=".$this->dbname;
-		//set options
+		// Set DSN
+		$dsn = 'mysql:host='. $this->host . ';dbname='. $this->dbname;
+		// Set Options
 		$options = array(
-			PDO::ATTR_PERSISTENT  	=> true,
+			PDO::ATTR_PERSISTENT		=> true,
 			PDO::ATTR_ERRMODE		=> PDO::ERRMODE_EXCEPTION
-			);
-		//create new PDO instance
-		try{
-			$this->dbh = new PDO($dsn, $this->user, $this->pw, $options);
-		} catch(PDOException $e){
-			$this->error = getMessage();
+		);
+		// Create new PDO
+		try {
+			$this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+		} catch(PDOEception $e){
+			$this->error = $e->getMessage();
 		}
 	}
 
 	public function query($query){
 		$this->stmt = $this->dbh->prepare($query);
-
 	}
 
 	public function bind($param, $value, $type = null){
@@ -38,28 +35,29 @@ class Database{
 				case is_int($value):
 					$type = PDO::PARAM_INT;
 					break;
-				case is_bool($value):	
+				case is_bool($value):
 					$type = PDO::PARAM_BOOL;
 					break;
-				case is_null($value):	
+				case is_null($value):
 					$type = PDO::PARAM_NULL;
 					break;
 					default:
 					$type = PDO::PARAM_STR;
-				
-
 			}
-
 		}
 		$this->stmt->bindValue($param, $value, $type);
 	}
+
 	public function execute(){
 		return $this->stmt->execute();
 	}
+
+	public function lastInsertId(){
+		$this->dbh->lastInsertId();
+	}
+
 	public function resultset(){
 		$this->execute();
 		return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
 }
-
-?>
--- ibex.php
+++ ibex.php
@@ -3,22 +3,36 @@ require 'classes/Database.php';
 
 $database = new Database;
 
-$database->query('SELECT * FROM posts WHERE id = :id');
-$database->bind(':id', 1);
-$rows = $database->resultset();
 
-if($_POST['submit']){
-	echo'SUBMITTED, BABY!';
+
+$post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+if($post['submit']){
+	$id = $post['id'];
+	$title = $post['title'];
+	$body = $post['body'];
+
+	$database->query('UPDATE posts SET title = :title, body = :body WHERE id = :id');
+	$database->bind(':title', $title);
+	$database->bind(':body', $body);
+	$database->bind(':id', $id);
+	$database->execute();
 }
+
+$database->query('SELECT * FROM posts');
+$rows = $database->resultset();
 ?>
 
 <h1>Add Posts</h1>
 <form method="post" action="<?php $_SERVER['PHP-SELF'];?>">
+	<label for="">Post ID</label><br/>
+	<input type="number" name="id" placeholder="Add an id number..." /><br />
 	<label for="">Post Title</label><br/>
-	<input type="text" name="title" placeholder="Add a Title..." /><br />
+	<input type="text" name="title" placeholder="Add a title..." /><br /><br />
 	<label for="">Post Body</label><br/>
-	<textarea name="body" id="" cols="30" rows="10"></textarea>
-	<input type="submit" name="submit" value="Submit"/>
+	<textarea name="body" id="" cols="" rows=""></textarea>
+	<input type="submit" name="submit" value="Submit" />
+	 
 </form>
 <h1>Posts</h1>
 <div>
